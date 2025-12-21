ea4fa871066066fb1927dab430e0fae310dfa59e
fbseongDev <fbseong.dev@gmail.com>
2025-12-21 13:14:37 +0900

    feat:: 전체 화면 v1

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+import 'display_screen.dart';
+
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -338,63 +340,6 @@ class ControllerPage extends HookWidget {
           ),
         );
       },
-    );
-  }
-}
-
-// -------------------- 2번 창 --------------------
-class DisplayApp extends StatelessWidget {
-  const DisplayApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: const DisplayPage());
-  }
-}
-
-class DisplayPage extends HookWidget {
-  const DisplayPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final value = useState<int>(0);
-
-    useEffect(() {
-      DesktopMultiWindow.setMethodHandler((call, fromWindowId) async {
-        if (call.method == 'update') {
-          value.value = call.arguments as int;
-        }
-      });
-      return null;
-    }, const []);
-
-    final slides = Slide.values;
-    return Material(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        children: [
-          Positioned.fill(
-            child: Image(
-              image: slides[value.value.clamp(0, slides.length - 1)].screen,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            margin: EdgeInsets.only(bottom: 16),
-            decoration: BoxDecoration(color: Colors.black.withAlpha(200)),
-            child: Text(
-              slides[value.value.clamp(0, slides.length - 1)].name,
-              style: TextStyle(
-
-                fontSize: MediaQuery.of(context).size.width/10,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
